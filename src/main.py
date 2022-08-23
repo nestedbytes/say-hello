dbeaa838c2435759b92b4b4240f9c44257e6c602
Shourjjo Majumder <shourjjomajum@outlook.com>
2022-08-23 14:33:06 +0530

    Good for production

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from kivy.uix.textinput import TextInput
 
 class SayHello(App):
     def build(self):
-        #returns a window object with all it's widgets
+
         self.window = GridLayout()
         self.window.cols = 1
         self.window.size_hint = (0.6, 0.7)
@@ -15,7 +15,7 @@ class SayHello(App):
 
 
 
-        # label widget
+     
         self.greeting = Label(
                         text= "What's your name?",
                         font_size= 18,
@@ -23,7 +23,7 @@ class SayHello(App):
                         )
         self.window.add_widget(self.greeting)
 
-        # text input widget
+  
         self.user = TextInput(
                     multiline= False,
                     padding_y= (20,20),
@@ -32,14 +32,12 @@ class SayHello(App):
 
         self.window.add_widget(self.user)
 
-        # button widget
         self.button = Button(
-                      text= "GREET",
+                      text= "Sumbit",
                       size_hint= (1,0.5),
                       bold= True,
                       background_color ='#00FFCE',
-                      #remove darker overlay of background colour
-                      # background_normal = ""
+                 
                       )
         self.button.bind(on_press=self.callback)
         self.window.add_widget(self.button)
@@ -47,9 +45,8 @@ class SayHello(App):
         return self.window
 
     def callback(self, instance):
-        # change label text to "Hello + user name!"
+        
         self.greeting.text = "Hello " + self.user.text + "!"
 
-# run Say Hello App Calss
 if __name__ == "__main__":
     SayHello().run()
